be45380aeacd3b6f6fcd0d0c47d358dcd3d97204
Damien Le Dantec <damien-le-dantec@hotmail.fr>
2024-07-20 18:12:17 +0200

    pwa

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,41 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Mes petits travaux",
+        short_name: "Travaux",
+        theme_color: "#ffffff",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "assets/android/192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "assets/android/512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "assets/android/512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   base: "./",
   server: {
     proxy: {
